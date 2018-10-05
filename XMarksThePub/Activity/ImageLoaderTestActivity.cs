1b03c9e3743ce5a26eeab751cbc7ecb73ff18b7f
Shael <p.patrik08@outlook.com>
2018-10-06 01:28:03 +0200

    ImageLoader
    Implement Image Loader and small clean up

---
+++ XMarksThePub/Activity/ImageLoaderTestActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace xMarksThePub.Activity
+{
+    [Activity(Label = "ImageLoaderTestActivity")]
+    public class ImageLoaderTestActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.TestLayout);
+
+            Button button = FindViewById<Button>(Resource.Id.change);
+
+            button.Click += delegate {
+                var imageView = FindViewById<ImageView>(Resource.Id.dolanImageView);
+
+                Bitmap dolan = BitmapFactory.DecodeResource(Resources, Resource.Drawable.Dolan);
+                ImageLoader.Instance.SaveImage(dolan, 1234);
+                Bitmap test = ImageLoader.Instance.LoadImage(1234);
+
+                imageView.SetImageBitmap(test);
+            };
+        }
+    }
+}
---
+++ XMarksThePub/Activity/MainActivity.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using xMarksThePub;
+using XMarksThePub.Model;
+
+namespace XMarksThePub
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        InterestType interestType;
+        Button start;
+        RadioButton pub;
+        RadioButton tobbaco;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            InitViews();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            AddEventHandlers();
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            RemoveEventHandlers();
+        }
+
+        private void InitViews()
+        {
+            start = FindViewById<Button>(Resource.Id.startButton);
+            pub = FindViewById<RadioButton>(Resource.Id.kocsmaRadioButton);
+            tobbaco = FindViewById<RadioButton>(Resource.Id.DohanyboltRadioButton);
+        }
+
+        private void AddEventHandlers()
+        {
+
+            start.Click += StartClick;
+            pub.Click += Kocsma_Click;
+            tobbaco.Click += Dohanybolt_Click;
+        }
+
+        private void RemoveEventHandlers()
+        {
+            start.Click -= StartClick;
+            pub.Click -= Kocsma_Click;
+            tobbaco.Click -= Dohanybolt_Click;
+        }
+
+        private void Kocsma_Click(object sender, EventArgs e)
+        {
+            interestType = InterestType.Pub;
+        }
+
+        private void Dohanybolt_Click(object sender, EventArgs e)
+        {
+            interestType = InterestType.Tobbaco;
+        }
+
+        private void StartClick(object sender, EventArgs e)
+        {
+            var intent = new Intent(this, typeof(ListActivity));
+            intent.PutExtra("InterestType", (int)interestType);
+
+            StartActivity(intent);
+        }
+    }
+}
+
